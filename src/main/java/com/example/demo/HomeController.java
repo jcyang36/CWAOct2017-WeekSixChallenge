7bb591dcfe3ded2227a61590c7a1b4b336cf660d
Joyce Yang <jcyang@hmc.edu>
2017-11-18 21:38:45 -0500

    Registration changes

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -4,16 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private UserService userService;
     @Autowired
     TransactionRepository transactionRepository;
 
@@ -21,9 +21,14 @@ public class HomeController {
     @RequestMapping("/")
     public String goIndex(Model model){
         model.addAttribute("transactions", transactionRepository.findAll());
-        return "index";
+        return "redirect:/register";
     }
 
+    @RequestMapping("/login")
+    public String login()
+    {
+        return "login";
+    }
     @RequestMapping("/list")
     public String goList(Model model){
         model.addAttribute("transactions", transactionRepository.findAll());
@@ -56,9 +61,39 @@ public class HomeController {
         model.addAttribute("transaction", transactionRepository.findOne(id));
         return "show";
     }
+
+@GetMapping("/register")
+public String showRegistrationPage(Model model){
+        model.addAttribute("user", new User());
+        return "register";
+
+}
+
+@PostMapping("/register")
+public String processRegistrationPage(@Valid @ModelAttribute("user") User user, BindingResult bindingResult, Model model){
+if(bindingResult.hasErrors()){
+    return "register";
+}else{
+    userService.saveUser(user);
+    model.addAttribute("message", "User Account Successfully Created");
+    return "index";
+}
+
+}
     @RequestMapping("/balance")
     public String goBalance(Model model){
         model.addAttribute("transactions", transactionRepository.findAll());
+        long balance=0;
+        for (Transaction transaction: transactionRepository.findAll()) {
+            if (transaction.getAction().equalsIgnoreCase("deposit")) {
+                balance=balance+transaction.getAmount();
+
+            }
+            else if (transaction.getAction().equalsIgnoreCase("withdrawal")){
+                balance=balance-transaction.getAmount();
+            }
+        }
+        model.addAttribute("balance", balance);
         return "balance";
     }
 
